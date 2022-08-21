27f331d43a70e63b8df4dbabf134b7e74a229dfc
thiseaskambas <tkambas@yahoo.com>
2022-08-21 15:59:27 +0300

    deploying to heroku

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
-morgan.token("postbody", (req, res) => {
-  return JSON.stringify(req.body);
-});
+// morgan.token("postbody", (req, res) => {
+//   return JSON.stringify(req.body);
+// });
 // app.use(
 //   morgan(
 //     ":method :url :status :res[content-length] - :response-time ms :postbody"
